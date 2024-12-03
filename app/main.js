d568466222a3bd49368769ac1a811b4f3ca2d7ab
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-12-02 21:37:20 -0500

    sdadas

--- app/main.js
+++ app/main.js
@@ -11,12 +11,14 @@ const DOMSelectors = {
 
 async function FFXI() {
     try {
-        const promise = await fetch("https://xivapi.com/item?columns=ID,Name,Description,LevelItem,ClassJobCategory.Name")
+        const promise = await fetch("https://ffxivcollect.com/api/spells")
         if (promise.status != 200) {
             throw new Error(promise)
         } else {
             const info = await promise.json();
-            allInfo(info.Results)
+            console.log(info.results[1]["aspect"]["name"]);
+            
+            allInfo(info.results)
         }
     } catch (error) {
         alert("No Page Found")
@@ -24,23 +26,23 @@ async function FFXI() {
 }
 
 function allInfo(array) {
-    array.forEach(item => {if(item.Name == "" || item.Description == "") {
-    } else {
-         addInfo(item);
-    }
-}
-)
+    array.forEach(item => addInfo(item))
 }
 
 function addInfo(stuff) {
-    DOMSelectors.tbody.insertAdjacentHTML(
+    DOMSelectors.container.insertAdjacentHTML(
         "beforeend",
-        `<tr>
-        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200>${stuff.ID}</td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">${stuff.Name}</td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">${stuff.Description}</td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">${stuff.LevelItem}</td>
-        </tr>`
+        `<div class="w-1/4 h-[33vw] m-4 px-2 flex flex-col justify-evenly items-center border border-gray-700 rounded-3xl bg-gray-800 text-center">
+          <h2 class="text-[3vw] my-3 text-orange-600">${stuff.name}</h2>
+          <div class="w-3/5 h-1/3 border border-gray-700 rounded-2xl overflow-hidden">
+            <img src="${stuff.icon}" alt="Card Image 2" class="w-full h-full object-cover">
+          </div>
+          <h2 class="text-[1vw] mb-1 text-orange-600">Rank ${stuff.rank}</h2>
+          <p class="m-2 text-[0.75vw]">${stuff.tooltip}</p>
+          <h2 class="text-[1vw] text-orange-600">Type: ${stuff.type["name"]}</h2>
+          <h2 class="text-[1vw] text-orange-600">Aspect: ${stuff.aspect["name"]}</h2>
+          <h2 class="text-[2vw] my-4 text-orange-600">${stuff.id}</h2>
+        </div>`
     )
 };
 
@@ -48,21 +50,3 @@ DOMSelectors.all.addEventListener("click", function(event) {
     FFXI();
     }
 ) 
-
-async function FFXI2() {
-    try {
-        const promise = await fetch("https://www.freetogame.com/api/games")
-        if (promise.status != 200) {
-            throw new Error(promise)
-        } else {
-            const info = await promise.json();
-            console.log(info)
-        }
-    } catch (error) {
-        alert("No Page Found")
-    }
-}
-
-const promise = await fetch("https://www.freetogame.com/api/games")
-const info = await promise.json();
-console.log(info)
